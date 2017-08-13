31f87ac90dca3c11fa57db84ce678bfd17f04878
minhdtb <minhdtb.83@gmail.com>
2017-08-13 22:48:54 +0700

    update coding

--- source/server/main.js
+++ source/server/main.js
@@ -1,7 +1,8 @@
-import {app, BrowserWindow} from 'electron'
+import {app, BrowserWindow, ipcMain} from 'electron'
 import path from 'path'
-import {ipcMain} from 'electron'
 
+const socket = require('net');
+let claymoreSocket;
 const exec = require('child_process').exec;
 let claymoreProcess;
 
@@ -21,6 +22,51 @@ function execClaymore(sender, params) {
     sender.send('response', {
         status: 'running'
     });
+}
+
+function connectToClaymore() {
+    claymoreSocket = socket.Socket();
+    claymoreSocket.setEncoding('ascii');
+
+    function ping() {
+        if (claymoreSocket.isConnected) {
+            claymoreSocket.write(JSON.stringify({
+                id: 0,
+                jsonrpc: "2.0",
+                method: "miner_getstat1"
+            }));
+
+            setTimeout(ping, 100);
+        }
+    }
+
+    claymoreSocket.connect(3333, 'localhost', () => {
+        claymoreSocket.isConnected = true;
+        ping();
+    });
+
+    claymoreSocket.on('data', d => {
+        processData(d)
+    });
+
+    claymoreSocket.on('close', () => {
+        claymoreSocket.isConnected = false;
+        setTimeout(connectToClaymore, 1000);
+    });
+
+    claymoreSocket.on('error', (e) => {
+        // do nothing
+    })
+}
+
+function killClaymore() {
+    exec('taskkill /PID ' + claymoreProcess.pid + ' /T /F');
+    claymoreProcess = null;
+}
+
+function processData(data) {
+    //  {"id": 0, "error": null, "result": ["9.7 - ETH", "4", "21102;2;0", "21102", "0;0;0", "off", "60;60", "eth-eu1.nanopool.org:9999", "0;0;0;0"]}
+    console.log(data);
 }
 
 app.on('ready', () => {
@@ -48,13 +94,14 @@ app.on('ready', () => {
     ipcMain.on('command', (event, data) => {
         if (data.command === 'start-mining') {
             execClaymore(event.sender, ['-epool', 'eth-eu1.nanopool.org:9999', '-ewal', '0x32590ccd73c9675a6fe1e8ce776efc2a287f5d12']);
+            connectToClaymore();
         } else if (data.command === 'stop-mining') {
-            exec('taskkill /PID ' + claymoreProcess.pid + ' /T /F');
-            claymoreProcess = null;
+            killClaymore();
         }
     })
 });
 
 app.on('window-all-closed', () => {
+    killClaymore();
     app.quit()
 });
